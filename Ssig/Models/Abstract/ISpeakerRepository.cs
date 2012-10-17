7ee2a93aa37cfafc2a9bae5a52f0d422c42e64ce
Russ Jester <rjester@gmail.com>
2012-10-17 15:53:35 -0400

    added speaker repository

---
+++ Ssig/Models/Abstract/ISpeakerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ssig.Models.Abstract {
+  public interface ISpeakerRepository {
+    Speaker Get(int id);
+    IQueryable<Speaker> GetAll();
+    Speaker Add(Speaker speaker);
+    Speaker Update(Speaker speaker);
+    void Delete(int speakerId);
+  }
+}
---
+++ Ssig/Models/Repositories/SpeakerRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Ssig.Models.Abstract;
+
+namespace Ssig.Models.Repositories
+{
+  public class SpeakerRepository : ISpeakerRepository {
+    private SsigContext _db { get; set; }
+
+    public SpeakerRepository() : this(new SsigContext()) {}
+    
+    public SpeakerRepository(SsigContext db) {
+      _db = db;
+    }
+    public Speaker Get(int id) {
+      return _db.Speakers.SingleOrDefault(m => m.Id == id);
+    }
+
+    public IQueryable<Speaker> GetAll() {
+      return _db.Speakers;
+    }
+
+    public Speaker Add(Speaker speaker) {
+      _db.Speakers.Add(speaker);
+      _db.SaveChanges();
+      return speaker;
+    }
+
+    public Speaker Update(Speaker speaker) {
+      _db.Entry(speaker).State = EntityState.Modified;
+      _db.SaveChanges();
+      return speaker;
+    }
+
+    public void Delete(int speakerId) {
+      var speaker = Get(speakerId);
+      _db.Speakers.Remove(speaker);
+      _db.SaveChanges();
+    }
+  }
+}
